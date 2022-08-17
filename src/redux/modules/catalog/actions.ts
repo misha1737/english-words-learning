ed8ab4f1b5830dad43e325dc986509f5959fbcc5
unknown <mikekozlov2@gmail.com>
2022-08-17 18:28:52 +0300

    integrated topics

--- src/redux/modules/catalog/actions.ts
+++ src/redux/modules/catalog/actions.ts
@@ -1,6 +1,5 @@
-import { put } from "redux-saga/effects";
-//import { collection, getDocs, doc, setDoc } from "firebase/firestore";
-//import { auth, db } from "./../../firebase";
+import { put, select } from "redux-saga/effects";
+import { getAPI, postAPI } from "./../../../utils/fetchData";
 import {
   loading,
   setError,
@@ -9,17 +8,14 @@ import {
   toOpenTopicModal,
   getCatalog,
 } from "./reducer";
+
 export function* workerCatalog(): Generator<any> {
   try {
-    // const colRef = collection(db, "blocks");
-    // const snapshots: any = yield getDocs(colRef);
-    // const docs = snapshots.docs.map((doc: any) => {
-    //   const data = doc.data();
-    //   data.id = doc.id;
-    //   return data;
-    // });
-    // yield put(setBlocks(docs));
-  } catch (err) {
+    const state: any = yield select();
+    const token = state.user.userData.token;
+    const response: any = yield getAPI("topic", token);
+    yield put(setBlocks(response.data));
+  } catch (err: any) {
     yield put(setError("Error loading catalog"));
   }
   yield put(loading(false));
@@ -29,13 +25,18 @@ export function* workerNewTopic(action: {
   payload: { name: string };
 }): Generator<any> {
   try {
-    // const citiesRef = collection(db, "blocks");
-    // yield setDoc(doc(citiesRef, "SF"), {
-    //   name: action.payload.name,
-    //   count: 0,
-    // });
-    // yield put(toOpenTopicModal(false));
-    // yield put(getCatalog());
+    const state: any = yield select();
+    const token = state.user.userData.token;
+    const name: string = action.payload.name;
+    const response: any = yield postAPI(
+      "topic",
+      {
+        name,
+      },
+      token
+    );
+    yield put(toOpenTopicModal(false));
+    yield put(getCatalog());
   } catch (err) {
     yield put(setError("Error loading topic"));
   }
--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,9 +1,15 @@
 import axios from "axios";
+import store from "./../redux";
+import { setUserData } from "./../redux/modules/user/reducer";
 axios.defaults.baseURL = "http://localhost:4999";
 export const postAPI = async (url: string, post: object, token?: string) => {
   try {
+    let jwt: string | boolean = false;
+    if (token) {
+      jwt = `Bearer ${token}`;
+    }
     const res = await axios.post(`/api/${url}`, post, {
-      headers: { Authorization: token || false },
+      headers: { Authorization: jwt },
     });
     return res;
   } catch (error: any) {
@@ -12,11 +18,21 @@ export const postAPI = async (url: string, post: object, token?: string) => {
 };
 
 export const getAPI = async (url: string, token?: string) => {
-  const res = await axios.get(`/api/${url}`, {
-    headers: { Authorization: token || false },
-  });
-
-  return res;
+  try {
+    let jwt: string | boolean = false;
+    if (token) {
+      jwt = `Bearer ${token}`;
+    }
+    const res = await axios.get(`/api/${url}`, {
+      headers: { Authorization: jwt },
+    });
+    return res;
+  } catch (error: any) {
+    if (error.response.status == 403) {
+      store.dispatch(setUserData(null));
+    }
+    return error.response;
+  }
 };
 
 export const patchAPI = async (url: string, post: object, token?: string) => {
